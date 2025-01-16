d25f832e3a172aa6777e0acb0b91a3719720d77e
Muhammad Ali Khan <alikhan236778@gmail.com>
2025-01-16 12:51:18 +0500

    further sub pipelines

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js"
@@ -9,7 +8,7 @@ dotenv.config({
 
 connectDB()
     .then(() => {
-        app.listen(process.env.PORT || 8000, () => {
+        app.listen(process.env.PORT || 3000, () => {
             console.log(` Server is running at port :${process.env.PORT}`);
         })
     })
